901cba6bb12d1403f39a89f1fee08f688347aa03
douglas <lupim2008@gmail.com>
2022-04-19 21:21:29 -0300

    Initial Commit

---
+++ ChallengeKlever/ChallengeKleverApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChallengeKleverApp.swift
+//  ChallengeKlever
+//
+//  Created by Douglas Biagi Grana on 19/04/22.
+//
+
+import SwiftUI
+
+@main
+struct ChallengeKleverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
