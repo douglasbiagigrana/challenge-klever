1dc0024ee5762196db7ea71c9143957f309607e6
douglas <lupim2008@gmail.com>
2022-04-21 16:43:15 -0300

    update my @main with environmentObjetct

--- ChallengeKlever/ChallengeKleverApp.swift
+++ ChallengeKlever/ChallengeKleverApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ChallengeKleverApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView().environmentObject(Network())
         }
     }
 }
