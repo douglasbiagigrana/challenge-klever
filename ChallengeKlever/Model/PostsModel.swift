8f0c238de175227f7a5815fbca715291299fa296
douglas <lupim2008@gmail.com>
2022-04-21 16:40:10 -0300

    create model

---
+++ ChallengeKlever/Model/PostsModel.swift
@@ -0,0 +1,14 @@
+//
+//  PostsModel.swift
+//  ChallengeKlever
+//
+//  Created by Douglas Biagi Grana on 20/04/22.
+//
+
+import Foundation
+
+struct PostsModel: Codable {
+    let id: String
+    let title: String
+    let description: String
+}
